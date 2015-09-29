fbac5a8bc0f65a29e1ff0f88ddd0f617cf8d69d9
samgerber <gerbyxvi@gmail.com>
2015-09-29 12:43:50 +0000

    Wrote program 02

--- coding-test-2/practice-problems/lib/02_letter_count.rb
+++ coding-test-2/practice-problems/lib/02_letter_count.rb
@@ -1,2 +1,24 @@
+# The function, `letter_count(str)` takes a string and
+# returns a hash mapping each letter to its frequency.
+# Ignores spaces.
+#
+#
+# Author::    Sam Gerber  (mailto:gerbyxvi@gmail.com)
+# Copyright:: Copyright (c) 2015
+# License::   Distributes under the same terms as Ruby
+
+
 def letter_count(str)
+  characters = str.downcase.split("")
+  hashes = {}
+  
+  characters.each do |character|
+    
+    if character >= "a" && character <= "z"
+      hashes[character] = hashes[character] ? hashes[character] + 1 : 1
+    end
+    
+  end
+  
+  hashes
 end
