8f9f815d0cdce0c3e14b76e05f79658d0a050422
samgerber <gerbyxvi@gmail.com>
2015-09-29 12:16:32 +0000

    Did coding-test-2's 00 and 01 problems.

--- coding-test-2/practice-problems/lib/00_nearest_larger.rb
+++ coding-test-2/practice-problems/lib/00_nearest_larger.rb
@@ -1,2 +1,36 @@
+# The function, `nearest_larger(arr, i)` takes an array and an
+# index.  The function then returns another index, `j`: satisfying:
+#
+# (a) `arr[i] < arr[j]`, AND
+# (b) there is no `j2` closer to `i` than `j` where `arr[i] < arr[j2]`.
+#
+# In case of ties (see example below), the earliest (left-most)
+# of the two indices will be returned. If no number in `arr` is 
+# larger than `arr[i]` return will be `nil`.
+#
+#
+# Author::    Sam Gerber  (mailto:gerbyxvi@gmail.com)
+# Copyright:: Copyright (c) 2015
+# License::   Distributes under the same terms as Ruby
+
+
 def nearest_larger(arr, idx)
+  return nil if arr.count < 2 || idx > arr.count - 1 || idx < 0
+  
+  distance = 1
+  
+  while idx - distance >= 0 || idx + distance < arr.count
+  
+    if idx - distance >= 0
+      return idx - distance if arr[idx-distance] > arr[idx]
+    end
+    
+    if idx + distance < arr.count
+      return idx + distance if arr[idx+distance] > arr[idx]
+    end
+
+    distance += 1
+  end
+  
+  nil
 end
--- coding-test-2/practice-problems/lib/01_no_repeats.rb
+++ coding-test-2/practice-problems/lib/01_no_repeats.rb
@@ -1,2 +1,36 @@
+# The function, `no_repeats(year_start, year_end)`,  takes a
+# range of years and outputs those years which do not have any
+# repeated digits.
+#
+#
+# Author::    Sam Gerber  (mailto:gerbyxvi@gmail.com)
+# Copyright:: Copyright (c) 2015
+# License::   Distributes under the same terms as Ruby
+
+
 def no_repeats(year_start, year_end)
+  return if year_start > year_end
+  results = []
+  
+  (year_start .. year_end).each do |year|
+    results << year if no_repeat?(year) 
+  end
+  
+  results
+end
+
+
+# Helper function, `no_repeat?(year)` returns true/false
+# if a given year doesn't have a repeat.
+
+def no_repeat?(year)
+  digits = year.to_s.split("")
+  counter = Array.new(10, 0) 
+  
+  digits.each do |digit|
+    counter[digit.to_i] += 1
+    return false if counter[digit.to_i] > 1
+  end
+  
+  true
 end
