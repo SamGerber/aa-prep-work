6c69835fa9f8c0577e95c42b3adf56904a41e70c
samgerber <gerbyxvi@gmail.com>
2015-09-29 16:26:47 +0000

    solved 03

--- coding-test-2/practice-problems/lib/03_ordered_vowels.rb
+++ coding-test-2/practice-problems/lib/03_ordered_vowels.rb
@@ -1,2 +1,29 @@
 def ordered_vowel_words(str)
+  result = []
+  
+  words = str.downcase.split
+  words.each do |word|
+    result << word if ordered_vowel_word?(word)
+  end
+  
+  result.join(" ")
+end
+
+def ordered_vowel_word?(word)
+  vowel = "a"
+  
+  word.each_char do |char|
+    
+    if is_vowel?(char)  
+      return false if char < vowel
+      vowel = char 
+    end
+    
+  end
+  
+  true
+end
+
+def is_vowel?(char)
+  char == "a" || char == "e" || char == "i" || char == "o" || char == "u"
 end
